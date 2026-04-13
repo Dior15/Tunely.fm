178aa5edab2b53b47a9e1344fcd3bae3ab9b5000
HTTime39 <n.s.dizon2005@outlook.com>
2026-04-13 13:21:15 -0400

    Added SongList component for basic template for displaying the song list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import SongList from "./components/SongList.vue"
 </script>
 
 <template>
   <HelloWorld />
+   <SongList />
 </template>
---
+++ src/components/SongList.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="box mt-3 mb-4 ml-6 mr-6">
+    <h1 class="title">Song List</h1>
+  
+    <div id="songList columns">
+      <div class="songTile box is-flex mb-1 p-1 has-text-left">
+        <span class="songTitle column is-half ">Title</span>
+        <span class="artist column">Artist</span>
+        <span class="album column">Album</span>
+        <span class="year column">Year</span>
+      </div>
+      <div class="songTile box is-flex p-1 has-text-left">
+        <span class="songTitle column is-half ">Title</span>
+        <span class="artist column">Artist</span>
+        <span class="album column">Album</span>
+        <span class="year column">Year</span>
+      </div>
+    </div>
+  </div>
+</template>
