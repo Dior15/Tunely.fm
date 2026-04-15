1b5c2dcbc818a91f02590c4e544d454daec009a8
HTTime39 <n.s.dizon2005@outlook.com>
2026-04-15 14:01:35 -0400

    Updated SongList to style currently playing song + action context to play buttons

--- client/src/components/SongList.vue
+++ client/src/components/SongList.vue
@@ -2,7 +2,7 @@
   import { onMounted } from "vue";
   import { usePlayer } from '../composables/usePlayer.js'
 
-  const { songs, fetchSongs, playSong } = usePlayer()
+  const { songs, currentSong, isPlaying, fetchSongs, playSong, togglePlay } = usePlayer()
 
   onMounted(fetchSongs)
 </script>
@@ -13,8 +13,13 @@
     <h1 class="title">Song List</h1>
   
     <div id="songList columns">
-      <div v-for="song in songs" class="songTile box is-flex mb-1 p-1 has-text-left">
-        <span class="songTitle column is-one-third"><button @click="playSong(song.id)"><font-awesome-icon icon="circle-play" /></button> {{ song.title }}</span>
+      <div v-for="song in songs" class="songTile box is-flex mb-1 p-1 has-text-left is-vcentered" :class="{'isPlaying': currentSong != null && currentSong.id == song.id}">
+        <span class="songTitle column is-one-third is-vcentered">
+          <button v-if="currentSong != null && currentSong.id == song.id && isPlaying" @click="togglePlay()"><font-awesome-icon  icon="pause" /></button>
+          <button v-else-if="currentSong != null && currentSong.id == song.id && !isPlaying" @click="togglePlay()"><font-awesome-icon  icon="play" /></button>
+          <button v-else @click="playSong(song.id)"><font-awesome-icon icon="play" /></button>
+          {{ song.title }}
+        </span>
         <span class="artist column is-one-quarter">{{ song.artist }}</span>
         <span class="album column is-one-quarter">{{ song.album }}</span>
         <span class="year column has-text-right">{{ song.year }}</span>
@@ -23,3 +28,19 @@
   </div>
 
 </template>
+
+<style scoped>
+  .songTile span {
+    white-space: nowrap;
+    overflow: auto;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+  }
+  .songTile span:-webkit-scrollbar {
+    display: none;
+  }
+
+  .isPlaying {
+    background-image: linear-gradient(to right, #9935e7, #14161a);
+  }
+</style>
