ebfc2a857061449c895b6263c7947da65b538ec3
naufil_ansari_05 <145029992+naufilansari05@users.noreply.github.com>
2026-04-15 14:58:11 -0400

    Frontend decoration
    Bunch of frontend changes, such as adding a navbar to the App.vue, allowing discreet navigation towards the static pages. The static pages also have their own stylesheet, making them more in line with the style presented in the Vue pages.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,49 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import SongList from "./components/SongList.vue"
+
+// navbar toggle based on screen-size
+import {ref} from 'vue'
+const isMenuActive = ref(false)
+
+function toggleMenu() {
+  isMenuActive.value = !isMenuActive.value
+}
+
 </script>
 
 <template>
+  <!-- Bulma style navbar -->
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <strong>Tunely.fm</strong>
+      </a>
+
+      <a 
+        role="button" 
+        class="navbar-burger" 
+        :class="{ 'is-active': isMenuActive }" 
+        aria-label="menu" 
+        aria-expanded="false" 
+        @click="toggleMenu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/">Home</a>
+        <a class="navbar-item" href="../public/pages/about.html">About</a>
+        <a class="navbar-item" href="../public/pages/contact.html">Contact Us</a>
+        <a class="navbar-item" href="../public/pages/info.html">Info</a>
+      </div>
+    </div>
+  </nav>
+
   <SongList />
   <HelloWorld />
 </template>
