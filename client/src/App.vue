d03352ef101fce5cbd50cb6c5800196d60a67c5b
Khristian Diorico <77899722+Dior15@users.noreply.github.com>
2026-04-15 16:23:55 -0400

    Basic nav bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,51 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import SongList from "./components/SongList.vue"
 import NowPlaying from './components/NowPlaying.vue'
-
-// navbar toggle based on screen-size
-import {ref} from 'vue'
-const isMenuActive = ref(false)
-
-function toggleMenu() {
-  isMenuActive.value = !isMenuActive.value
-}
+import NavBar from './components/NavBar.vue'
 
 </script>
 
 <template>
   
   <div id="tunely-app">
-    <!-- Bulma style navbar -->
-    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <strong>Tunely.fm</strong>
-        </a>
-  
-        <a 
-          role="button" 
-          class="navbar-burger" 
-          :class="{ 'is-active': isMenuActive }" 
-          aria-label="menu" 
-          aria-expanded="false" 
-          @click="toggleMenu"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-  
-      <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
-        <div class="navbar-start">
-          <a class="navbar-item" href="/">Home</a>
-          <a class="navbar-item" href="../public/pages/about.html">About</a>
-          <a class="navbar-item" href="../public/pages/contact.html">Contact Us</a>
-          <a class="navbar-item" href="../public/pages/info.html">Info</a>
-        </div>
-      </div>
-    </nav>
+    <NavBar />
     <main class="tunely-main">
       <SongList />
     </main>
